2aaabce3d09c0d4c5f9e8a2a55f53fb7a4ecc61d
Prajit <prazitstha22@gmail.com>
2024-01-10 17:41:32 +0545

    changes

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@
  */
 
 import type {PropsWithChildren} from 'react';
+import Modal from 'react-native-modal';
 import {
   Image,
   Platform,
@@ -22,9 +23,12 @@ import React, {useEffect, useRef, useState} from 'react';
 import {ActivityIndicator, Linking} from 'react-native';
 import {
   Camera,
+  Templates,
   useCameraDevice,
   // useCameraDevice,
   useCameraDevices,
+  useCameraFormat,
+  useCodeScanner,
   useFrameProcessor,
 } from 'react-native-vision-camera';
 import {
@@ -38,13 +42,32 @@ import {
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
+const data = {
+  bhairab: {
+    id: '12345',
+    name: 'Bhairav',
+    desc: 'Bhairava originates from the word bhīru, which means "fearsome". Bhairava means "terribly fearsome form". It is also known as one who destroys fear or one who is beyond fear. One interpretation is that he protects his devotees from dreadful enemies, greed, lust, and anger. These enemies are dangerous as they never allow humans to seek God within. There is also another interpretation: Bha means creation, ra means sustenance and va means destruction. Therefore, Bhairava is the one who creates, sustains and dissolves the three stages of life. Therefore, he becomes the ultimate or the supreme.',
+    img: require('./assets/bhairav.jpg'),
+  },
+  hanuman: {
+    id: '22212',
+    name: 'Hanuman',
+    desc: 'Hanuman (/ˈhʌnʊˌmɑːn/; Sanskrit: हनुमान्, IAST: Hanumān),[5] also known as Maruti, Bajrangabali, and Anjaneya,[6] is a deity in Hinduism, revered as a divine vanara and a devoted companion of the deity Rama. Central to the Ramayana, Hanuman is celebrated for his unwavering devotion to Rama and is considered a chiranjivi. He is traditionally believed to be the spiritual offspring of the wind deity Vayu, who is said to have played a significant role in his birth.[7][8] His tales are recounted not only in the Ramayana but also in the Mahabharata and various Puranas.',
+    img: require('./assets/hanuman.jpg'),
+  },
+};
 
 function App(): JSX.Element {
   const camera = useRef(null);
   const [hasPermission, setHasPermission] = useState(false);
   const [showCamera, setShowCamera] = useState(false);
   const [imageSource, setImageSource] = useState('');
-
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [enableOnCodeScanned, setEnableOnCodeScanned] = useState(true);
+  const [selectedData, setSelectedData] = useState({});
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   //   frame => {
   //     'worklet';
   //     if (model.state !== 'loaded') return;
@@ -81,6 +104,21 @@ function App(): JSX.Element {
     console.log('hello');
     // Frame processing logic
   }, []);
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: codes => {
+      // if (enableOnCodeScanned) {
+      let value = codes[0]?.value;
+      console.log('value', value);
+      if (value === 'bhairav') {
+        setSelectedData(data.bhairab);
+        setModalVisible(true);
+      } else if (value === 'hanuman') {
+        setSelectedData(data.hanuman);
+        setModalVisible(true);
+      }
+    },
+  });
   useEffect(() => {
     async function getPermission() {
       const permission = await Camera.requestCameraPermission();
@@ -97,7 +135,12 @@ function App(): JSX.Element {
       console.log(photo.path);
     }
   };
-  const device = useCameraDevice('back');
+  const [cameraType, setCameraType] = useState('back');
+  const device = useCameraDevice(cameraType);
+  const format = useCameraFormat(device, [
+    {videoResolution: {width: 3048, height: 2160}},
+    {fps: 60},
+  ]);
   // const device = devices.back;
 
   // if (!hasPermission) {
@@ -123,13 +166,30 @@ function App(): JSX.Element {
             ref={camera}
             style={StyleSheet.absoluteFill}
             device={device}
+            format={format}
             isActive={showCamera}
             photo={true}
-            frameProcessor={frameProcessor}
-            // frameProcessorFps={1}
+            // frameProcessor={frameProcessor}
+            codeScanner={codeScanner}
+            exposure={0}
           />
 
           <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={() =>
+                cameraType === 'back'
+                  ? setCameraType('front')
+                  : setCameraType('back')
+              }>
+              <Text
+                style={{
+                  backgroundColor: '#FFFFFF',
+                  marginRight: 20,
+                  padding: 10,
+                }}>
+                Switch
+              </Text>
+            </TouchableOpacity>
             <TouchableOpacity
               style={styles.camButton}
               onPress={() => capturePhoto()}
@@ -199,6 +259,41 @@ function App(): JSX.Element {
           </View>
         </>
       )}
+      <Modal
+        isVisible={isModalVisible}
+        onSwipeComplete={() => setModalVisible(false)}
+        style={{display: 'flex', justifyContent: 'flex-end', margin: 0}}
+        swipeDirection="left">
+        <View style={{backgroundColor: '#FFFFFF'}}>
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              padding: 10,
+            }}>
+            <Text style={{fontSize: 20, fontWeight: '800'}}>X</Text>
+          </TouchableOpacity>
+          <View
+            style={{
+              alignItems: 'center',
+              paddingHorizontal: 10,
+              paddingTop: 10,
+              paddingBottom: 30,
+            }}>
+            {console.log('ssssssssssssssssssss', selectedData)}
+            <Image
+              source={selectedData?.img}
+              style={{height: 150, width: 150}}
+            />
+            <Text style={{textAlign: 'center', fontSize: 18, paddingTop: 10}}>
+              {selectedData?.name}
+            </Text>
+            <Text style={{fontSize: 16}}>{selectedData?.desc}</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -223,6 +318,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     backgroundColor: 'rgba(0,0,0,0.2)',
     position: 'absolute',
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
